0854327f6408a9e9d87b6db73d4a5ddb59e2f7e8
ah3200 <ah3200@nyu.edu>
2015-06-24 19:48:32 -0400

    initial commit

---
+++ twitterdownload.py
@@ -0,0 +1,30 @@
+# Arnnop Hualchareonthon (ah3200)
+#Import the necessary methods from tweepy library
+import tweepy
+import sys
+import json
+
+#Access Token and Access Token Secret of Twitter API (apps)
+access_token = "298785729-0kXfAarOFfx7XvraFTYaXYXQQqGEZzQX0kxWaqX5"
+access_token_secret = "lcv1XHrWy3FglHTSujF0nFpdNCo9y1WssXbcF5PW4D1Fn"
+
+if __name__ == '__main__':
+
+#   To run program: python twitterdownload.py CONSUMER_KEY CONSUMER_SECRET
+    consumer_key = sys.argv[1]
+    consumer_secret = sys.argv[2]
+    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+
+    public_tweets = api.home_timeline()
+
+    with open('twitterdata.csv','w') as f:
+        for tweet in public_tweets:
+            print str(tweet.id_str) + str(tweet.created_at) + tweet.text
+            f.write(str(tweet.id_str) + ',' + str(tweet.created_at) + ',' + tweet.text.encode("utf-8") + '\n')
+    f.closed
+
+    
